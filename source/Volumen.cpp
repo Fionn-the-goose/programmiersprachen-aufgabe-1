6fc4a891228804d199aeb2bcc3e7fc588fbd698b
YoFionn <ericksoncooperation@gmail.com>
2020-05-09 21:33:12 +0200

    CMakeLists.txt

---
+++ source/Volumen.cpp
@@ -0,0 +1,26 @@
+#define CATCH_CONFIG_RUNNER
+#include <catch.hpp>
+#include <cmath>
+#include <math.h>
+
+
+
+float volumen_zylinder(float radius, float hight)
+{
+	float volumen = M_PI * radius^2 * hight;
+	return volumen;
+}
+
+
+
+TEST_CASE("describe_volumen_zylinder", "[volumen_zylinder]")
+{
+	REQUIRE(fract(3.0, 2.0) == Approx(18.849555));
+}
+
+
+
+int main(int argc, char* argv[])
+{
+	return Catch::Session().run(argc, argv);
+}
