5b4e839aaf8739b4e96ae5ae9b9804f1712ffcad
YoFionn <ericksoncooperation@gmail.com>
2020-05-09 22:34:40 +0200

    ../../source/factorial.cpp

---
+++ source/factorial.cpp
@@ -0,0 +1,27 @@
+#define CATCH_CONFIG_RUNNER
+#include <catch.hpp>
+#include <cmath>
+
+int factorial(int a)
+{
+	int counter = 0;
+	for (int i = 1; i >= a; i++)
+	{
+		counter = counter * i;
+	}
+	return counter;
+}
+
+
+
+TEST_CASE("describe_factorial", "[factorial]")
+{
+	REQUIRE(factorial(7) == 5040);
+}
+
+
+
+int main(int argc, char* argv[])
+{
+	return Catch::Session().run(argc, argv);
+}
