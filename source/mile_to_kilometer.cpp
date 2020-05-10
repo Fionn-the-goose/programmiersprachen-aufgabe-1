abb027ffbd0409051a6d5223df14c5f629320e7c
YoFionn <ericksoncooperation@gmail.com>
2020-05-10 14:01:34 +0200

    ../../source/mile_to_kilometer.cpp

--- source/mile_to_kilometer.cpp
+++ source/mile_to_kilometer.cpp
@@ -13,6 +13,7 @@ double mile_to_kilometer(double mi)
 int main(int argc, char* argv[])
 {
 	std::cout << "Geben sie einen Meilenwert ein zum Umrechnen in Kilometer:\n";
+	int mile = 0;
 	std::cin >> mile;
 	std::cout << "In Kilometern ist das: ";
 	std::cout << mile_to_kilometer(mile);
